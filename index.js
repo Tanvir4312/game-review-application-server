d97b90f090eb977ec389600805d0ebba8140e7d0
Tanvirul islam <tanveer99ryan@gmail.com>
2025-03-06 01:47:17 +0600

    post methode add and reviews data get

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.h2tkvzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,16 +17,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const reviewsCollection = client.db("All-Review").collection("Review");
+
+    app.get("/reviews", async (req, res) => {
+      const games = await reviewsCollection.find().toArray();
+      res.send(games);
+    });
+
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      console.log(review);
+      const result = await reviewsCollection.insertOne(review);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -39,12 +51,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("The Game Review application server");
+});
 
-
-app.get('/', (req, res) =>{
-    res.send('The Game Review application server')
-})
-
-app.listen(port, ()=>{
-    console.log('The game review application running on port',port);
-})
+app.listen(port, () => {
+  console.log("The game review application running on port", port);
+});
